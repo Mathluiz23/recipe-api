7e0e9b9c4121334814b007c9c89198cef217ef93
Matheus Luiz da Silva <matheus_luizs@hotmail.com>
2023-01-29 17:26:56 -0300

    :sparkles: feat Implementando as funções ReadAll e  ReadOne

--- src/recipes-api/Controllers/RecipesController.cs
+++ src/recipes-api/Controllers/RecipesController.cs
@@ -12,43 +12,57 @@ namespace recipes_api.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class RecipesController : ControllerBase
-{    
-    public readonly IRecipeService _service;
-    
-    public RecipesController(IRecipeService service)
+{
+  public readonly IRecipeService _service;
+
+  public RecipesController(IRecipeService service)
+  {
+    this._service = service;
+  }
+
+  //Read
+  [HttpGet]
+  public IActionResult Get()
+  {
+    var recipes = _service.GetRecipes();
+
+    if (recipes == null)
     {
-        this._service = service;        
+      return NotFound();
     }
 
-    //Read
-    [HttpGet]
-    public IActionResult Get()
+    return Ok(recipes);
+  }
+
+  //Read
+  [HttpGet("{name}", Name = "GetRecipe")]
+  public IActionResult Get(string name)
+  {
+    var recipe = _service.GetRecipe(name);
+
+    if (recipe == null)
     {
-        throw new NotImplementedException();    
+      return NotFound();
     }
 
-    //Read
-    [HttpGet("{name}", Name = "GetRecipe")]
-    public IActionResult Get(string name)
-    {                
-        throw new NotImplementedException();
-    }
+    return Ok(recipe);
+  }
 
-    [HttpPost]
-    public IActionResult Create([FromBody]Recipe recipe)
-    {
-        throw new NotImplementedException();
-    }
+  [HttpPost]
+  public IActionResult Create([FromBody] Recipe recipe)
+  {
+    throw new NotImplementedException();
+  }
 
-    [HttpPut("{name}")]
-    public IActionResult Update(string name, [FromBody]Recipe recipe)
-    {
-        throw new NotImplementedException();
-    }
+  [HttpPut("{name}")]
+  public IActionResult Update(string name, [FromBody] Recipe recipe)
+  {
+    throw new NotImplementedException();
+  }
 
-    [HttpDelete("{name}")]
-    public IActionResult Delete(string name)
-    {
-        throw new NotImplementedException();
-    }    
+  [HttpDelete("{name}")]
+  public IActionResult Delete(string name)
+  {
+    throw new NotImplementedException();
+  }
 }
